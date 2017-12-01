01c4f6762333fafca8a95b8d2edbd18cceaa99a9
Bob Michaels <rom@jlab.org>
2017-12-01 12:12:00 -0500

    improved comparisons

--- CheckT.C
+++ CheckT.C
@@ -10,7 +10,7 @@
    gROOT->ForceStyle();
 
    // 0 = hamc vs HRSTRANS
-   // 1 = HRSTRANS vs LeRose's warmseptum 1st order
+   // 1 = hamc vs LeRose's warmseptum 1st order
 
    Int_t which=0; 
 
@@ -19,40 +19,50 @@
      TH2F *h3x = new TH2F("h3x","X (HRSTrans) vs X (LeRose fcn)",100,-1.0,0.08,100,-1.0,0.08);
      TH2F *h2th = new TH2F("h2th","tan(th) HRSTrans vs LeRose fcn",100,-0.08,0.04,100,-0.08,0.04);
      TH2F *h3th = new TH2F("h3th","tan(th) HRSTrans vs LeRose fcn",100,-0.08,0.04,100,-0.08,0.04);
-     TH2F *h2y = new TH2F("h2y","Y (HRSTrans) vs Y (LeRose fcn)",100,-0.1,0.1,100,-0.1,0.1);
-     TH2F *h3y = new TH2F("h3y","Y (HRSTrans) vs Y (LeRose fcn)",100,-0.1,0.1,100,-0.1,0.1);
+     TH2F *h2y = new TH2F("h2y","Y (HRSTrans) vs Y (LeRose fcn)",100,-0.08,0.08,100,-0.08,0.08);
+     TH2F *h3y = new TH2F("h3y","Y (HRSTrans) vs Y (LeRose fcn)",100,-0.08,0.08,100,-0.08,0.08);
      TH2F *h2ph = new TH2F("h2ph","tan(phi) HRSTrans vs LeRose fcn",100,-0.04,0.04,100,-0.04,0.04);
      TH2F *h3ph = new TH2F("h3ph","tan(phi) HRSTrans vs LeRose fcn",100,-0.04,0.04,100,-0.04,0.04);
    } else {
-     TH2F *h2x = new TH2F("h2x","X (HRSTrans) vs X (LeRose 1st Order)",100,-1.0,0.08,100,-1.0,0.08);
-     TH2F *h3x = new TH2F("h3x","X (HRSTrans) vs X (LeRose 1st Order)",100,-1.0,0.08,100,-1.0,0.08);
-     TH2F *h2th = new TH2F("h2th","tan(th) HRSTrans vs LeRose 1st Order",100,-0.08,0.04,100,-0.08,0.04);
-     TH2F *h3th = new TH2F("h3th","tan(th) HRSTrans vs LeRose 1st Order",100,-0.08,0.04,100,-0.08,0.04);
-     TH2F *h2y = new TH2F("h2y","Y (HRSTrans) vs Y (LeRose 1st Order)",100,-0.1,0.1,100,-0.1,0.1);
-     TH2F *h3y = new TH2F("h3y","Y (HRSTrans) vs Y (LeRose 1st Order)",100,-0.1,0.1,100,-0.1,0.1);
-     TH2F *h2ph = new TH2F("h2ph","tan(phi) HRSTrans vs LeRose 1st Order",100,-0.04,0.04,100,-0.04,0.04);
-     TH2F *h3ph = new TH2F("h3ph","tan(phi) HRSTrans vs LeRose 1st Order",100,-0.04,0.04,100,-0.04,0.04);
+     TH2F *h2x = new TH2F("h2x","X (LeRose fcn) vs X (LeRose 1st Order)",100,-1.0,0.08,100,-1.0,0.08);
+     TH2F *h3x = new TH2F("h3x","X (LeRose fcn) vs X (LeRose 1st Order)",100,-1.0,0.08,100,-1.0,0.08);
+     TH2F *h2th = new TH2F("h2th","tan(th) LeRose fcn vs LeRose 1st Order",100,-0.08,0.04,100,-0.08,0.04);
+     TH2F *h3th = new TH2F("h3th","tan(th) LeRose fcn vs LeRose 1st Order",100,-0.08,0.04,100,-0.08,0.04);
+     TH2F *h2y = new TH2F("h2y","Y (LeRose fcn) vs Y (LeRose 1st Order)",100,-0.08,0.08,100,-0.08,0.08);
+     TH2F *h3y = new TH2F("h3y","Y (LeRose fcn) vs Y (LeRose 1st Order)",100,-0.08,0.08,100,-0.08,0.08);
+     TH2F *h2ph = new TH2F("h2ph","tan(phi) LeRose fcn vs LeRose 1st Order",100,-0.04,0.04,100,-0.04,0.04);
+     TH2F *h3ph = new TH2F("h3ph","tan(phi) LeRose fcn vs LeRose 1st Order",100,-0.04,0.04,100,-0.04,0.04);
    }
 
+   Float_t zloc = -1.4;
+   char strplot[100];
+ 
    if (which==0) {
-     hrt->Draw("outx:hamcx>>h2x");
-     hrt->Draw("outx:hamcx>>h3x","wsdp>-0.002");
+     sprintf(strplot,"outx:hamcx-%f*hamcth>>h2x",zloc);
+     hrt->Draw(strplot);
+     sprintf(strplot,"outx:hamcx-%f*hamcth>>h3x",zloc);
+     hrt->Draw(strplot,"wsdp>-0.002");
      hrt->Draw("outth:hamcth>>h2th");
      hrt->Draw("outth:hamcth>>h3th","wsdp>-0.002");
-     hrt->Draw("outy:hamcy>>h2y");
-     hrt->Draw("outy:hamcy>>h3y","wsdp>-0.002");
+     sprintf(strplot,"outy:hamcy-%f*hamcph>>h2y",zloc);
+     hrt->Draw(strplot);
+     sprintf(strplot,"outy:hamcy-%f*hamcph>>h3y",zloc);
+     hrt->Draw(strplot,"wsdp>-0.002");
      hrt->Draw("outph:hamcph>>h2ph");
      hrt->Draw("outph:hamcph>>h3ph","wsdp>-0.002");
    } else {
-     Float_t zextra=1.43;
-     hrt->Draw("outx:wsx-1.43*wsth>>h2x");
-     hrt->Draw("outx:wsx-1.43*wsth>>h3x","wsdp>-0.002"); 
-     hrt->Draw("outth:wsth>>h2th");
-     hrt->Draw("outth:wsth>>h3th","wsdp>-0.002");
-     hrt->Draw("outy:wsy-1.43*wsph>>h2y");
-     hrt->Draw("outy:wsy-1.43*wsph>>h3y","wsdp>-0.002");
-     hrt->Draw("outph:wsph>>h2ph");
-     hrt->Draw("outph:wsph>>h3ph","wsdp>-0.002");
+     sprintf(strplot,"hamcx:wsx-%f*wsth>>h2x",zloc);
+     hrt->Draw(strplot);
+     sprintf(strplot,"hamcx:wsx-%f*wsth>>h3x",zloc);
+     hrt->Draw(strplot,"wsdp>-0.002");
+     hrt->Draw("hamcth:wsth>>h2th");
+     hrt->Draw("hamcth:wsth>>h3th","wsdp>-0.002");
+     sprintf(strplot,"hamcy:wsy-%f*wsph>>h2y",zloc);
+     hrt->Draw(strplot);
+     sprintf(strplot,"hamcy:wsy-%f*wsph>>h3y",zloc);
+     hrt->Draw(strplot,"wsdp>-0.002");
+     hrt->Draw("hamcph:wsph>>h2ph");
+     hrt->Draw("hamcph:wsph>>h3ph","wsdp>-0.002");
   }
 
    h3x->SetMarkerColor(2);
